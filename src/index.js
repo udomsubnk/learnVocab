ed74d49d50f5504e8ede75d4e4f52f3f792f09cd
Your Name <you@example.com>
2017-03-18 20:59:03 +0700

    Add Navbar,Content,Footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Navbar from './Navbar';
+import Footer from './Footer'
 import './index.css';
 
 ReactDOM.render(
+  <Navbar />,
+  document.getElementById('navbar')
+);
+ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('content')
+);
+ReactDOM.render(
+  <Footer />,
+  document.getElementById('footer')
 );
